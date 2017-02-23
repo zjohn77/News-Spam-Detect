f77c1c399c00dd01da020b2744e14532ddafbe75
John Jung <zjohn77@gmail.com>
2017-02-22 19:33:29 -0500

    initial commit

--- prototype/Archive/view/Parser2.py
+++
@@ -1,4 +0,0 @@
-### 5. Check that TfidfVectorizer, by default, normalized each doc vector 
-###    so that it's inscribed in the unit hypersphere.
-kmeansFit1.labels_
-kmeansFit1.cluster_centers_.shape
--- prototype/Archive/viz/plot.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-
-def chart_dict(d):	
-	lists = sorted(d.items()) # sorted by key, return a list of tuples
-	x, y = zip(*lists) # unpack a list of pairs into two tuples
-	plt.plot(x, y)
-	plt.show()
