0596fb42552145a689a415eaa80a03b38d2040b2
John Jung <zjohn77@gmail.com>
2017-03-09 18:52:21 -0500

    cleaned up dir

--- Archive/view/present/present_results.py
+++
@@ -1,6 +0,0 @@
-def labeledList_to_dict(clustered_txt_, sentences_): 
-    '''Display each cluster label followed by data points belonging to that cluster. '''
-    clusterDict = collections.defaultdict(list)
-    for idx, label in enumerate(clustered_txt_):
-        clusterDict[label].append(sentences_[idx])
-    return clusterDict
